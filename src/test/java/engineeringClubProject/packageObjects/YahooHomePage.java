0e6dd849ca86f99efb2c37f1d3af1024f1a53f31
SemenchenkoYaroslav <semenchenko.yaroslav@icloud.com>
2023-08-11 13:18:15 +0300

    Refactor git repository

--- src/test/java/engineeringClubProject/packageObjects/YahooHomePage.java
+++
@@ -1,9 +0,0 @@
-package engineeringClubProject.packageObjects;
-
-import org.openqa.selenium.WebDriver;
-
-public class YahooHomePage extends BasePage{
-    public YahooHomePage(WebDriver driver) {
-        super(driver);
-    }
-}
